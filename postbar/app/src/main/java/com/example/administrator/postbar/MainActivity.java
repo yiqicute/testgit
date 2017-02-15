3d672eef7cd8d87c72f1bc730a275ca749461932
yiqicute <302818696@qq.com>
2017-02-15 22:46:52 +0800

    完善登录注册

--- postbar/app/src/main/java/com/example/administrator/postbar/MainActivity.java
+++ postbar/app/src/main/java/com/example/administrator/postbar/MainActivity.java
@@ -1,31 +1,52 @@
 package com.example.administrator.postbar;
 
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button signinbt;
-    private EditText user_name,password;
-    private TextView register;
+    private Button signin_bt;
+    private EditText user_name_et,user_password_et;
+    private CharSequence user_name,user_password;
+    private TextView register_enter;
+    interface Checkpassword {
+        void checkpassword(CharSequence user_name,CharSequence user_password);
+    }
+    class Client implements Checkpassword {
+        public void checkpassword(CharSequence user_name,CharSequence user_password) {
+            //这里写后台登录接口方法
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        user_name = (EditText)findViewById(R.id.editText);
-        password = (EditText)findViewById(R.id.editText2);
-        signinbt = (Button)findViewById(R.id.button);
-        signinbt.setOnClickListener(new View.OnClickListener() {
+        user_name_et = (EditText)findViewById(R.id.editText);
+        user_password_et = (EditText)findViewById(R.id.editText2);
+        signin_bt = (Button)findViewById(R.id.button);
+        signin_bt.setOnClickListener(new View.OnClickListener() {
             @Override
-                public void onClick(View v){
-
+                public void onClick(View v) {
+                    user_password = user_password_et.getText();
+                    user_name = user_name_et.getText();
+                    new Client().checkpassword(user_name,user_password);
             }
-        }
+        });
+
+        register_enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View v) {
+                    Intent intent_register = new Intent(MainActivity.this,register.class);
+                    startActivity(intent_register);
+            }
+        });
     }
 }
